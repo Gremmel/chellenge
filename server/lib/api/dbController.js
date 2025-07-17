648b453561f5f81f678e41758dd7759192150517
Gremmel <mc@soseies.de>
2025-07-17 13:47:43 +0200

    ie

--- server/lib/api/dbController.js
+++ server/lib/api/dbController.js
@@ -13,7 +13,33 @@ const __dirname = path.dirname(__filename);
 class DatabaseController {
   constructor (dbPath) {
     this.db = new Database(dbPath, { verbose: console.log });
-    logger.fatal('constructor db');
+    this.ensureChallengeColumns();
+    logger.info('Datenbankverbindung hergestellt und Spalten überprüft.');
+  }
+
+  /**
+   * Prüft, ob die gewünschten Spalten in der Tabelle challenges existieren und fügt sie ggf. hinzu.
+   */
+  ensureChallengeColumns () {
+    const columnsToAdd = [
+      { name: 'challengeTyp', type: 'INTEGER' },
+      { name: 'countBeginn', type: 'INTEGER' },
+      { name: 'countAdd', type: 'INTEGER' },
+      { name: 'countIntervall', type: 'INTEGER' }
+    ];
+
+    // Hole die existierenden Spaltennamen
+    const pragma = this.db.prepare('PRAGMA table_info(challenges)').all();
+    const existingColumns = pragma.map((col) => col.name);
+
+    columnsToAdd.forEach((col) => {
+      if (!existingColumns.includes(col.name)) {
+        const sql = `ALTER TABLE challenges ADD COLUMN ${col.name} ${col.type}`;
+
+        this.db.prepare(sql).run();
+        logger.info(`Spalte '${col.name}' zur Tabelle 'challenges' hinzugefügt.`);
+      }
+    });
   }
 
   prepare (query) {
